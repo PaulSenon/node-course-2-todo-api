358e53e1a4c74967d054d1e169fe5b16c66c5050
PaulSenon <paul.senon@iut2.upmf-grenoble.fr>
2017-05-30 19:11:21 +0200

    First look to mangoose

---
+++ server/server.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
+
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+//
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
+
+var newTodo = new Todo({
+    text: 'Feed the cat',
+    completed: true,
+    completedAt: 123
+});
+
+newTodo.save().then((res) => {
+    console.log(res);
+}, (err) => {
+    console.log('Unable to save todo');
+})
