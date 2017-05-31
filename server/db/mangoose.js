26af1cce71a86eb81dbf43a122143554f0ebf32e
PaulSenon <paul.senon@iut2.upmf-grenoble.fr>
2017-05-31 21:46:22 +0200

    Add GET /todos route

--- server/db/mangoose.js
+++ server/db/mangoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp').catch((e) => {
+    console.log('Unable to connect to mongo DB');
+});
 
 
 module.exports = {
