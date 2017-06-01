b0780caac44f5ce8f3259bd9792bb927d771f194
PaulSenon <paul.senon@iut2.upmf-grenoble.fr>
2017-06-01 19:34:39 +0200

    Add POST /users route and token method

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 
+// when heroku run this, the NODE_ENV is elready set to 'production'
+// so next lines are skipped, and MONGODB_URI is already set.
 if (env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
